e96d83a6ccc20b5074fb475fc4e69531b53cf808
Jeeyong Um <conr2d@proton.me>
2024-12-05 15:38:42 +0900

    chore(deps): Bump curve25519-dalek to 4.1.3

--- sdk/program/src/pubkey.rs
+++ sdk/program/src/pubkey.rs
@@ -174,6 +174,7 @@ pub fn bytes_are_curve_point<T: AsRef<[u8]>>(_bytes: T) -> bool {
     #[cfg(not(target_os = "solana"))]
     {
         curve25519_dalek::edwards::CompressedEdwardsY::from_slice(_bytes.as_ref())
+            .unwrap()
             .decompress()
             .is_some()
     }
